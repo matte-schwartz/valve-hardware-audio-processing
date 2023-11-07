4b58dd77b8045f93b97465b890e16b5f8f5bd9eb
Ethan Geller <ethang@valvesoftware.com>
2023-11-07 11:21:15 -0800

    Fix for our mic tuning plugin regressing.

--- setup_pipewire.sh
+++ setup_pipewire.sh
@@ -4,8 +4,8 @@ destination_dir=$DEST_DIR/usr/share/pipewire/pipewire.conf.d
 echo "setting up pipewire configuration."
 mkdir -p $destination_dir
 cp -r pipewire-confs/filter-chain-speakers.conf $destination_dir/filter-chain-sink.conf
-# cp -r pipewire-confs/filter-chain-mic.conf      $destination_dir/filter-chain.conf 
+cp -r pipewire-confs/filter-chain-mic.conf      $destination_dir/filter-chain.conf
 cp -r pipewire-confs/virtual-sink.conf          $destination_dir/virtual-sink.conf
-# cp -r pipewire-confs/virtual-source.conf        $destination_dir/virtual-source.conf
+cp -r pipewire-confs/virtual-source.conf        $destination_dir/virtual-source.conf
 echo "pipewire setup complete. reboot your machine."
 
