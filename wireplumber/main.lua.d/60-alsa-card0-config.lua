eb980781dc9b9eded32f04183c7c02d06ff6acef
Ethan Geller <ethang@valvesoftware.com>
2023-11-22 22:21:14 +0000

    remove period_size. As talked about previously, this doesn't actually invoke any alsa ctls, and just causes source+sink enumeration over pulse to provide incorrect information about our callback size.

--- wireplumber/main.lua.d/60-alsa-card0-config.lua
+++ wireplumber/main.lua.d/60-alsa-card0-config.lua
@@ -18,7 +18,6 @@ table.insert (alsa_monitor.rules, {
   apply_properties = {
     ["priority.driver"]        = 900,
     ["priority.session"]       = 900,
-    ["api.alsa.period-size"]   = 256,
     ["api.alsa.headroom"]      = 1024,
     ["session.suspend-timeout-seconds"] = 0
   }
