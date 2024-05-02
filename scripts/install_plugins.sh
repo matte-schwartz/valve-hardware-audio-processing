8e0fb7962863ef9a63902ae517a63080366e915a
John Schoenick <johns@valvesoftware.com>
2024-05-02 16:28:47 -0700

    Stop shipping various development files under lv2 dir
    The SVGs are big.  Lv2 tries to load all these as plugins and gets
    sad, also.

--- scripts/install_plugins.sh
+++ scripts/install_plugins.sh
@@ -9,5 +9,5 @@ mkdir -p $DEST_DIR/usr/lib/lv2
 cp -r bin/lv2/g++/valve_deck_speakers.lv2 $DEST_DIR/usr/lib/lv2/valve_deck_speakers.lv2
 cp -r bin/lv2/g++/valve_binaural.lv2 $DEST_DIR/usr/lib/lv2/valve_binaural.lv2
 cp -r bin/lv2/g++/valve_deck_microphone.lv2 $DEST_DIR/usr/lib/lv2/valve_deck_microphone.dsp
-cp -r bin/svg $DEST_DIR/usr/lib/lv2/svg
-cp version.txt $DEST_DIR/usr/lib/lv2/valve_audio_dsp_version.txt
+# cp -r bin/svg $DEST_DIR/usr/lib/lv2/svg
+# cp version.txt $DEST_DIR/usr/lib/lv2/valve_audio_dsp_version.txt
